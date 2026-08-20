18a2f1e96bd7877a4e95b8246e7c9b2b95be40eb
KOU050223 <kouhei20050223@icloud.com>
2026-08-21 07:23:05 +0900

    chore: nixを使った開発環境周りの整備

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "WIPプロジェクト";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      inherit (inputs.nixpkgs) lib;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem =
+        f:
+        lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              self.formatter.${system}
+              # 開発言語
+              pkgs.go
+              pkgs.nodejs_24
+
+              # 開発ツール
+              pkgs.go-task
+              pkgs.lefthook
+            ];
+          };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
